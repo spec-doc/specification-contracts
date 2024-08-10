7a8cd01208523d2f5ec53bbf2bd26bad465ca6c3
Ivan Mezinov <ivanmezinov@mail.ru>
2024-08-11 02:57:26 +0300

    add Specification blank

---
+++ Specification/SpecificationInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SpecDoc\Specificator\Contracts\Specification;
+
+/**
+ * Specification must know which elements a document can consist of, contain rules
+ * for construction and interaction of elements with each other.
+ */
+interface SpecificationInterface {}
